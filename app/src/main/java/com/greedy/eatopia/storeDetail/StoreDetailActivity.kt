772ea979aad792b6e16351c98c5089fae10bf72d
Shinkju <tlsrudwn1103@gmail.com>
2022-10-27 19:40:11 +0900

    파일생성

---
+++ app/src/main/java/com/greedy/eatopia/storeDetail/StoreDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.greedy.eatopia.storeDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greedy.eatopia.R
+
+class StoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_detail)
+    }
+}
---
+++ app/src/main/java/com/greedy/eatopia/user/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.greedy.eatopia.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greedy.eatopia.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
